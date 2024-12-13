d18af21539e2857e470fa2214e82e26eb7454e09
HyeonsuLee <leehyeonsu4888@naver.com>
2024-12-13 17:07:52 +0900

    feat: 날짜를 나타내는 Model 작성

---
+++ src/main/java/oncall/model/Date.java
@@ -0,0 +1,24 @@
+package oncall.model;
+
+import oncall.exception.Exceptions;
+
+public class Date {
+    private final Month month;
+    private final int day;
+
+    public Date(Month month, int day) {
+        validateDay(month, day);
+        this.month = month;
+        this.day = day;
+    }
+
+    public DayOfWeek getDayOfWeek() {
+        return DayOfWeek.values()[(day + month.getFirstDayOfWeek().ordinal()) % 7];
+    }
+
+    private void validateDay(Month month, int day) {
+        if (day < 0 || day > month.countDays()) {
+            throw Exceptions.INPUT_FORMAT_EXCEPTION.getException();
+        }
+    }
+}
