9a526f6f74f6533bd770e23c263130fc377f2b14
HyeonsuLee <leehyeonsu4888@naver.com>
2024-12-13 16:48:29 +0900

    feat: 프로젝트 기본 구조 작성

--- src/main/java/oncall/exception/Exceptions.java
+++ src/main/java/oncall/exception/Exceptions.java
@@ -2,7 +2,7 @@ package oncall.exception;
 
 public enum Exceptions {
 
-    PRICE_NUMBER_FORMAT_EXCEPTION(new IllegalArgumentBaseException("가격은 숫자여야 합니다.")),
+    INPUT_FORMAT_EXCEPTION(new IllegalArgumentBaseException("유효하지 않은 입력 값입니다. 다시 입력해 주세요.")),
     ;
 
     private final IllegalArgumentBaseException exception;
